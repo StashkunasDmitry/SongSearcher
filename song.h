5fe78ba58a7ff86da87994bd5a0078519bb1b662
StashkunasDmitry <48912914+StashkunasDmitry@users.noreply.github.com>
2019-09-06 19:55:35 +0700

    Create song.h

---
+++ song.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Windows.h>
+#include <clocale>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <list>
+
+void search(ifstream &file );	
+void add(ifstream &file);	
+void deleteSong(ifstream &file);		
+void show();		
+//объявляем функции для song.cpp
