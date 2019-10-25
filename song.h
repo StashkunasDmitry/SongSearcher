0f6454d4fe14c4474a0e88a422f1ff2fb8c091ad
StashkunasDmitry <48912914+StashkunasDmitry@users.noreply.github.com>
2019-10-25 09:56:11 +0700

    Update song.h

--- song.h
+++ song.h
@@ -1,14 +1,17 @@
 #pragma once
-#include <Windows.h>
 #include <clocale>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <string>
 #include <list>
+#include <ostream>
+#include "menu.h"
+void search(ifstream &file);
+void add(ifstream &file);
+void deleteSong(ifstream &file);
+void show();
+void transpose();
+void clearscreen();
 
-void search(ifstream &file );	
-void add(ifstream &file);	
-void deleteSong(ifstream &file);		
-void show();		
 //объявляем функции для song.cpp
